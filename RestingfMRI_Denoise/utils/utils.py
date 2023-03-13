bdd6d928b2981f0df012434242b9b0c937c565ee
XiaoXiaoqian <33399278+XiaoXiaoqian@users.noreply.github.com>
2023-03-13 16:57:13 -0700

    Update utils.py

--- RestingfMRI_Denoise/utils/utils.py
+++ RestingfMRI_Denoise/utils/utils.py
@@ -2,6 +2,10 @@ from os.path import exists
 import json
 import jsonschema
 import copy
+import typing as t
+import re
+from bids.layout import parse_file_entities, writing
+
 
 _pipeline_valid_keys = ["name", "descrtiption", "confounds"]
 type_checker = jsonschema.Draft4Validator.VALIDATORS
@@ -21,6 +25,12 @@ def load_pipeline_from_json(json_path: str) -> dict:
             js = json.load(json_file)
     js = swap_booleans(js, inplace=True)
     return js
+
+def extract_pipeline_from_path(path: str) -> str:
+    match = re.search(r'(?<=pipeline-)(.*?)(?=_)', path)
+    if match is not None:
+        return match.group()
+    return ""
 
 def is_booleanlike(value) -> bool:
     """
@@ -76,8 +86,48 @@ def swap_booleans(dictionary: dict, inplace: bool=True) -> dict:  # TODO: Extend
 def is_IcaAROMA(pipeline: dict) -> bool:
     return cast_bool(pipeline["aroma"])
 
+
+def parse_file_entities_with_pipelines(filename, entities=None, config=None,
+                                       include_unmatched=False) -> t.Dict[str, str]:
+    """
+    bids.extract_pipelines_from_path extended with ability to
+    """
+    et_dict = parse_file_entities(filename, entities, config, include_unmatched)
+    pipeline = extract_pipeline_from_path(filename)
+    if pipeline:
+        et_dict['pipeline'] = pipeline
+    return et_dict
+
+
+def is_entity_subset(entity_superset: t.Dict[str, str], entity_subset: t.Dict[str, str]) -> bool:
+    """
+    Checks if all key values in subset are in superset
+    Args:
+        entity_superset: bigger dict
+        entity_subset: smaller dict
+    Returns: true if all key-values pairs from entity_subset are in entity_superset
+    """
+    return all(entity_superset.get(entity_key) == entity_value for entity_key, entity_value in entity_subset.items())
+
+
+def build_path(entities, path_patterns, strict=False):
+    path = writing.build_path(entities, path_patterns, strict)
+    if path is not None:
+        return path
+    else:
+        raise ValueError(f"Unable to build path with given entities: {entities}\n and path pattern {path_patterns}")
+
+
+def assert_all_entities_equal(entities: t.List[t.Dict[str, str]], *entities_names: str) -> None:
+    if len(entities) == 0:
+        return
+    for name in entities_names:
+        first = entities[0].get(name)
+        if any(entity.get(name) != first for entity in entities):
+            raise AssertionError(f"Not all entities equal for key: {name}\n"
+                                 f"{[entitie.get(name) for entitie in entities]}")
+
 if __name__ == '__main__':
-    #  rudimentary test/proof of work
     dicto = load_pipeline_from_json("../pipelines/pipeline-24HMP_8Phys_spikes-FD2.json")
     print(dicto)
     print(swap_booleans(dicto, True))
